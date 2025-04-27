b0389a6d8ca412a9e84ce16433c70e12e94cb9aa
wongsakron kongkamud <wongsakronkongkamud@gmail.com>
2025-04-28 00:00:31 +0700

    complete destination desktop page

--- src/pages/Destination.jsx
+++ src/pages/Destination.jsx
@@ -8,11 +8,11 @@ const Destination = () => {
   return (
     <div className="relative min-h-screen overflow-auto">
       <div className="h-screen w-full bg-[url(src/assets/destination/background-destination-mobile.jpg)] bg-no-repeat bg-center bg-cover fixed inset-0 -z-10"></div>
-      <section className="mt-36 text-center p-6">
-        <h3 className="text-white">
-          <span>01</span> PICK YOUR DESTINATION
+      <section className="mt-36 text-center p-6 lg:grid lg:grid-cols-2 lg:text-left">
+        <h3 className="text-white lg:absolute lg:top-2/12 lg:left-1/12 lg:text-4xl">
+          <span className="text-slate-500">01</span> PICK YOUR DESTINATION
         </h3>
-        <section className="pt-8 flex justify-center">
+        <section className="pt-8 flex justify-center lg:pt-52">
           <img
             src={
               new URL(
@@ -21,11 +21,11 @@ const Destination = () => {
               ).href
             }
             alt="star"
-            className="w-8/12"
+            className="w-8/12  lg:w-auto"
           />
         </section>
-        <article className="text-white pt-8">
-          <ol className="flex gap-8 justify-center text-slate-400">
+        <article className="text-white pt-8 lg:pr-40 lg:pt-52">
+          <ol className="flex gap-8 justify-center lg:justify-start text-slate-400">
             {destinations.map((_, i) => (
               <li
                 className={`cursor-pointer ${
@@ -42,7 +42,7 @@ const Destination = () => {
             <p className="text-slate-400 pt-4 ">{current.description}</p>
           </div>
           <hr className="border-gray-700 mt-8" />
-          <div className="text-white grid grid-cols-2 pt-8">
+          <div className="text-white grid grid-cols-2 lg:grid-cols-[0.7fr_1fr] pt-8">
             <div>
               <h3 className="text-slate-400 text-sm">AVG.DISTANCE</h3>
               <h2 className="text-2xl">{current.distance}</h2>
